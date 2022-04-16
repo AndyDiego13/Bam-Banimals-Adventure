aec2886fa5cab25ecacc9648472b37db609ee54c
Andy Diego <andyserrano_d@outlook.com>
2022-04-15 23:40:04 -0500

    changes in trigger code

--- Bam Banimals Adventure/Assets/Scripts/Game/trigger.cs
+++ Bam Banimals Adventure/Assets/Scripts/Game/trigger.cs
@@ -17,6 +17,26 @@ public class trigger : MonoBehaviour {
     private void Start() 
     {
         moveSpeed = GameObject.Find("GameManager").GetComponent<MoveSpeed>();
+        tiles = GameObject.Find("Manager").GetComponent<MakeList>();
+        circle = GameObject.Find("circle").GetComponent<CircleMove>();
+        AroundCircle = GameObject.Find("circle").GetComponent<Circle>();
+        Change = false;
+        tileNum = 0;
+        speed = moveSpeed.speed;
+    }
+
+    private void OnTriggerStay2D(Collider2D other) 
+    {
+        circle.isMove = true;
+
+        for (int i = 0; i < tiles.map.Count; i++)
+        {
+            if (circle.moveDot.transform.position == tiles.map[i].transform.position)
+            {
+                tileNum = 1;
+                break;
+            }
+        }
         
     }
 }
