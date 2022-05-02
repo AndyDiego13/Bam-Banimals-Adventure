d1ddbefc9fa94a82ba3e357c0933f7f8e9fe38cd
Andy Diego <andyserrano_d@outlook.com>
2022-05-02 14:54:17 -0500

    after several changes i turn back

--- Bam Banimals Adventure/Assets/Scripts/demo/WaveSpawn.cs
+++ Bam Banimals Adventure/Assets/Scripts/demo/WaveSpawn.cs
@@ -12,6 +12,40 @@ public class Wave
 }
 public class WaveSpawn : MonoBehaviour
 {
+    //all the waves we ever gonna have
     public Wave[] waves;
     public Transform[] spawnPoints;
+
+    private Wave currentWave;
+    // check in what wave are you current in
+    private int currentWaveNumber;
+    private float nextSpawnTime;
+
+    private bool canSpawn = true;
+    private void Update() 
+    {
+        currentWave = waves[currentWaveNumber];
+        SpawnWave();
+        GameObject[] totalBubbles = GameObject.FindGameObjectsWithTag("bubbles");
+        if (totalBubbles.Length == 0 && !canSpawn && currentWaveNumber + 1 != waves.Length)
+        {
+            currentWaveNumber++;
+            canSpawn = true;
+        }
+    }
+    void SpawnWave()
+    {
+        if(canSpawn && nextSpawnTime < Time.time)
+        {
+            GameObject randomEnemy = currentWave.typeOfEnemies[Random.Range(0, currentWave.typeOfEnemies.Length)];
+            Transform randomPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            Instantiate(randomEnemy, randomPoint.position, Quaternion.identity);
+            currentWave.numOfBubbles--;
+
+            if (currentWave.numOfBubbles == 0)
+            {
+                canSpawn = false;
+            }
+        }
+    }
 }
